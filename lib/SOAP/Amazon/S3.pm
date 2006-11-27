2bbb437e6b0290b8953479cb16185d9f0e8b7e28
Alexander Karelas <karjala@karjala.org>
2006-11-27 12:55:14 +0000

    Import of KARJALA/SOAP-Amazon-S3-0.023 from CPAN.
    gitpan-cpan-distribution: SOAP-Amazon-S3
    gitpan-cpan-version:      0.023
    gitpan-cpan-path:         KARJALA/Amazon/SOAP-Amazon-S3-0.023.tar.gz
    gitpan-cpan-author:       KARJALA
    gitpan-cpan-maturity:     released

--- lib/SOAP/Amazon/S3.pm
+++ lib/SOAP/Amazon/S3.pm
@@ -16,11 +16,11 @@ SOAP::Amazon::S3 - A module for interfacing with Amazon S3 through SOAP
 
 =head1 VERSION
 
-Version 0.022
+Version 0.023
 
 =cut
 
-our $VERSION = '0.022';
+our $VERSION = '0.023';
 
 =head1 SYNOPSIS
 
